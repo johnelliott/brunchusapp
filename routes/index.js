de9b08d20c2c0614932943319e4cf57f77d0da7a
John Elliott <johnelliott703@gmail.com>
2016-09-27 17:34:22 -0400

    Add a weird proxy to couchDB for JSON requests

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,24 @@ const upload = multer()
 
 const recs = JSON.parse(fs.readFileSync(`lib/recommendations.json`))
 
-/* GET outing. */
-router.get('/outings/:magicLinkId/recs', function (req, res, next) {
-  debug(`temp recommendations: are just a ${typeof recs}: ${JSON.stringify(recs, null, 2)}`)
-  res.json(recs)
-})
-/* GET outing. */
-router.get('/outing', function (req, res, next) {
-  res.render('outing')
+const url = require('url')
+const request = require('request')
+
+// proxy to couchdb if the request warrants it
+router.use(function couchDBDirectMiddleware (req, res, next) {
+  // if (req.acceptsEncodings('application/json'))
+  if (req.accepts('application/json')) {
+    debug('this is totaly proxyable to couchdb')
+    if (req.url.match(/\/api/)) {
+      const urlParts = url.parse(req.url)
+      const apiBase = '/api'
+      const urlResolved = url.resolve(apiBase, `http://127.0.0.1:${process.env.COUCH_PORT}${urlParts.path.slice(apiBase.length)}`)
+      // req.headers.set('')
+      req.pipe(request(urlResolved)).pipe(res)
+    } else {
+      next()
+    }
+  }
 })
 
 /* POST form submission. */
@@ -28,14 +38,25 @@ router.post('/outings', upload.array(), createOuting)
 /* GET an outing as JSON data. */
 router.get('/outings/:outing', fetchOuting)
 
-/* POST like for a recommendation. */
-router.post('/outings/:outing/like/:placeId', function (req, res, next) {
-  res.send('sunrise/sunset')
+/* GET outing page. */
+/* GET outing. */
+router.get('/outing', function (req, res, next) {
+  res.render('outing', {parties: ['yolo', 'wegotyou']})
 })
 
+// router.get('/outings/:magicLinkId/recs', function (req, res, next) {
+//   debug(`temp recommendations: are just a ${typeof recs}: ${JSON.stringify(recs, null, 2)}`)
+//   res.render('outing', {parties: 'yolo', 'trolo'})
+// })
+
+/* POST like for a recommendation. */
+// router.post('/outings/:outing/like/:placeId', function (req, res, next) {
+//   res.send('sunrise/sunset')
+// })
+
 /* POST pass for a recommendation. */
-router.post('/outings/:outing/pass/:placeId', function (req, res, next) {
-  res.send('sunrise/sunset')
-})
+// router.post('/outings/:outing/pass/:placeId', function (req, res, next) {
+//   res.send('sunrise/sunset')
+// })
 
 module.exports = router
