0ee2677dafa829e072fa8db7996137609e63fa90
John Elliott <johnelliott703@gmail.com>
2016-09-27 17:58:09 -0400

    Server JSON from couchdb by request header

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,21 @@
-const fs = require('fs')
-// const crypto = require('crypto')
+const debug = require('debug')('brunch.us:api-service:routes')
 const express = require('express')
 const multer = require('multer')
-const debug = require('debug')('brunch.us:api-service:routes')
+const request = require('request')
 const createOuting = require('./outings/create')
 const fetchOuting = require('./outings/fetch')
 
 const router = express.Router()
 const upload = multer()
 
-const recs = JSON.parse(fs.readFileSync(`lib/recommendations.json`))
-
-const url = require('url')
-const request = require('request')
-
 // proxy to couchdb if the request warrants it
 router.use(function couchDBDirectMiddleware (req, res, next) {
   // if (req.acceptsEncodings('application/json'))
   if (req.accepts('application/json')) {
-    debug('this is totaly proxyable to couchdb')
-    if (req.url.match(/\/api/)) {
-      const urlParts = url.parse(req.url)
-      const apiBase = '/api'
-      const urlResolved = url.resolve(apiBase, `http://127.0.0.1:${process.env.COUCH_PORT}${urlParts.path.slice(apiBase.length)}`)
-      // req.headers.set('')
-      req.pipe(request(urlResolved)).pipe(res)
-    } else {
-      next()
-    }
+    debug(`this is totaly proxyable to couchdb accepts: ${req.accepts('application/json')} is: ${req.is('application/json')}`)
+    req.pipe(request(`http://127.0.0.1:${process.env.COUCH_PORT}${req.path}`)).pipe(res)
+  } else {
+    next()
   }
 })
 
@@ -46,7 +34,7 @@ router.get('/outing', function (req, res, next) {
 
 // router.get('/outings/:magicLinkId/recs', function (req, res, next) {
 //   debug(`temp recommendations: are just a ${typeof recs}: ${JSON.stringify(recs, null, 2)}`)
-//   res.render('outing', {parties: 'yolo', 'trolo'})
+  res.render('outing', {parties: ['yolo', 'trolo']})
 // })
 
 /* POST like for a recommendation. */
