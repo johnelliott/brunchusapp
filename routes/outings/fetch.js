2f844cf0ad1875ba99c9d5a9ac60a70f9be9fc6f
John Elliott <johnelliott703@gmail.com>
2016-10-04 13:12:27 -0400

    Use magic links to render outing info

--- routes/outings/fetch.js
+++ routes/outings/fetch.js
@@ -12,17 +12,21 @@ const outings = nano.use('outings')
 // responds with JSON
 module.exports = function fetchOuting (req, res, next) {
   debug('request body', req.body)
-  const outingId = req.params.outing
+  const magicLinkId = req.params.outing
+  console.log(`magicLinkId ${magicLinkId}`)
   // TODO check request type and serce as page or JSON
 
-  const outingRequest = outings.get(outingId, function (err, body) {
+  // /outings/_design/findOuting/_view/byMagicLink
+  outings.view('findOuting', 'byMagicLink', {reduce: false, key: magicLinkId}, function (err, body) {
     if (err) {
       debug.error(err)
       next(err)
     }
     debug('COUCHDB fetched a body', body)
-    // debug(`temp recommendations: are just a ${typeof recs}: ${JSON.stringify(recs, null, 2)}`)
-    // res.status(200)
-    // res.end(body)
+    // debug(`temp recommendations: are just a ${typeof borecs}: ${JSON.stringify(recs, null, 2)}`)
+    res.status(200)
+    res.render('outing', {
+      data: JSON.stringify(body.rows[0].value.createdAt, null, 2)
+    })
   })
 }
