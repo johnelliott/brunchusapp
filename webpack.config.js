5bb8571bd2efe862649e7f0530adf2e3c318bb4b
John Elliott <johnelliott703@gmail.com>
2016-10-11 13:19:21 -0400

    Update production webpack config for react

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     // http://webpack.github.io/docs/multiple-entry-points.html
     form: path.join(__dirname, 'client/index.js'),
     cards: path.join(__dirname, 'client/cards.js'),
+    app: path.join(__dirname, 'client/app.js'),
     style: path.join(__dirname, 'client/style.js'),
   },
   output: {
@@ -25,7 +26,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['react', 'es2015']
         }
       },
       {
