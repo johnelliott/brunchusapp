9b4a0f46057162b9761043178e474bee1468d35e
John Elliott <johnelliott703@gmail.com>
2016-10-12 19:10:00 -0400

    Fix warning for cards transitionGroup

--- client/components/stack.js
+++ client/components/stack.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Card from './card'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
-
 const Stack = React.createClass({
   render () {
     return (
@@ -10,11 +9,11 @@ const Stack = React.createClass({
         {/* https://facebook.github.io/react/docs/jsx-spread.html#spread-attributes */ }
         <ReactCSSTransitionGroup
           transitionName='example'
-          transitionAppear='true'
+          transitionAppear={true}
           transitionAppearTimeout={1500}
           transitionEnterTimeout={1500}
           transitionLeaveTimeout={1300}>
-          {this.props.cards.map(props => <Card className='card' key={props.id} {...props.place} />)}
+          {this.props.cards.map(props => <Card className='card' ref={`card-key-${props.id}`} key={props.id} {...props.place} />)}
         </ReactCSSTransitionGroup>
       </div>
     )
