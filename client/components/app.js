96f823d706528a39e6c9594b163f60ead2147c4d
John Elliott <johnelliott703@gmail.com>
2016-10-14 00:15:59 -0400

    Put non-component concerns outside components

--- client/components/app.js
+++ client/components/app.js
@@ -1,53 +1,11 @@
 // TODO eventually copy this into ../cards.js and replace that file
-
 require('../cards.scss')
 
 import React from 'react'
-import { render } from 'react-dom'
-
 import Stack from './stack'
 import Buttons from './buttons'
 
-console.log('cards.js is running')
-
-// This might as well come from an API...
-const cards = [
-  {
-    'id': 1,
-    'place': {
-      'placeId': 1,
-      'name': 'sunrise/sunset',
-      'distance': '0.5mi',
-      'dollarSigns': 2
-    }
-  },
-  {
-    'id': 2,
-    'place': {
-      'placeId': 2,
-      'name': 'little skips',
-      'distance': '0.7mi',
-      'dollarSigns': 2
-    }
-  }
-]
-
-const handlers = {
-  pass: function passHandler (evt) {
-    console.log('pass handler called')
-    cards.pop() // take off the top card
-    // TODO post like this thing
-    renderApp()
-  },
-  like: function likeHandler (evt) {
-    console.log('like handler called')
-    cards.pop() // take off the top card
-    // TODO post like this thing
-    renderApp()
-  }
-}
-const appProps = { cards, handlers }
-
+// Compose buttons and cards
 const App = React.createClass({
   render () {
     return (<div className='container'>
@@ -56,10 +14,4 @@ const App = React.createClass({
     </div>)
   }
 })
-
-function renderApp () {
-  render(<div><App {...appProps} /></div>, document.getElementById('root'))
-}
-
-renderApp()
 export default App
--- client/react-app.js
+++ client/react-app.js
@@ -1,2 +1,50 @@
 console.log('app.js is running')
-require('./components/app.js')
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
+
+console.log('cards.js is running')
+
+// This might as well come from an API...
+const cards = [
+  {
+    'id': 1,
+    'place': {
+      'placeId': 1,
+      'name': 'sunrise/sunset',
+      'distance': '0.5mi',
+      'dollarSigns': 2
+    }
+  },
+  {
+    'id': 2,
+    'place': {
+      'placeId': 2,
+      'name': 'little skips',
+      'distance': '0.7mi',
+      'dollarSigns': 2
+    }
+  }
+]
+
+const handlers = {
+  pass: function passHandler (evt) {
+    console.log('pass handler called')
+    cards.pop() // take off the top card
+    // TODO post like this thing
+    renderApp()
+  },
+  like: function likeHandler (evt) {
+    console.log('like handler called')
+    cards.pop() // take off the top card
+    // TODO post like this thing
+    renderApp()
+  }
+}
+const appProps = { cards, handlers }
+
+function renderApp () {
+  render(<div><App {...appProps} /></div>, document.getElementById('root'))
+}
+
+renderApp()
